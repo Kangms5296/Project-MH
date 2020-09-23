2a3fe43ed333667db3c1507860e69a91d476d410
Kangms5296 <rlfhfhdhk@naver.com>
2020-09-23 15:50:36 +0900

    Project start!!

---
+++ Source/ProjectMH/ProjectMHGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectMHGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMH_API AProjectMHGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
